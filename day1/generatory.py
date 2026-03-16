19f423789afe55dc8e336d3c5d53915131d52b16
rajkonkret <rajkonkret660@gmail.com>
2026-03-16 13:25:04 +0100

    [1 12] add day1

--- day1/generatory.py
+++ day1/generatory.py
@@ -15,7 +15,7 @@ print(liczby())
 
 def gen_liczby():
     for i in range(31):
-        yield i
+        yield i  # zwraca wartość, zapamiętuje krok w którym zakończył
 
 
 print(gen_liczby())  # <generator object gen_liczby at 0x0000023ACE598FB0>
@@ -25,3 +25,14 @@ print(list(gen_liczby()))
 for p in gen_liczby():
     print(p)
     time.sleep(1)
+
+gen = gen_liczby()
+print(next(gen))
+print(next(gen))
+print("Inna operacja")
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+
+# StopIteration
