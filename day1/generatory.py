135ca2a828497a5192b0405a96add75716d4a1b3
rajkonkret <rajkonkret660@gmail.com>
2026-03-16 13:29:59 +0100

    [1 14] add generatory.py

--- day1/generatory.py
+++ day1/generatory.py
@@ -24,7 +24,7 @@ print(list(gen_liczby()))
 
 for p in gen_liczby():
     print(p)
-    time.sleep(1)
+    # time.sleep(1)
 
 gen = gen_liczby()
 print(next(gen))
@@ -35,4 +35,37 @@ print(next(gen))
 print(next(gen))
 print(next(gen))
 
+
 # StopIteration
+
+def wznowienie(n, k):
+    print("Wstrzymanie działania")
+    yield 3005
+
+    print("Wznowienie działania 1")
+    yield n + k
+
+    print("Wznowienie działania 2")
+    n = 8 * k - 4
+    yield n - k
+
+    print("Wznowienie działanai 3")
+    yield n * k
+
+    print("Wznowienie działania 4")
+    yield n / k
+
+    print("Wznowienie działania 5")
+
+
+print(wznowienie(8, 4))
+
+print(list(wznowienie(8, 4)))
+# <generator object wznowienie at 0x0000025B39D98FB0>
+# Wstrzymanie działania
+# Wznowienie działania 1
+# Wznowienie działania 2
+# Wznowienie działanai 3
+# Wznowienie działania 4
+# Wznowienie działania 5
+# [3005, 12, 24, 112, 7.0]
