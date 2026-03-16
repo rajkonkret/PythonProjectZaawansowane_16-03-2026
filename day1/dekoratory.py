1d8b269aebb2b617341aced00344a1ea5384246b
rajkonkret <rajkonkret660@gmail.com>
2026-03-16 15:07:32 +0100

    [1 23] update dekoratory.py

--- day1/dekoratory.py
+++ day1/dekoratory.py
@@ -10,3 +10,14 @@ def pomiarczasu(funkcja):
         print(f"Czas wykonania funkcji: {wynik} s.")
 
     return wrapper
+
+
+# usypiacz. opóznia wykonanie o 3 sekundy, zwraca wynik działąnia funkcji
+def usypiacz(funkcja):
+    def wrapper():
+        time.sleep(3)
+        return funkcja()
+
+    return wrapper
+
+
