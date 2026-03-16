2a5a495d8feb42aba865abf96967d9ce2702907b
rajkonkret <rajkonkret660@gmail.com>
2026-03-16 15:03:09 +0100

    [1 22] add funkcja_w_funkcji.py

---
+++ day1/dekoratory.py
@@ -0,0 +1,12 @@
+import time
+
+
+def pomiarczasu(funkcja):
+    def wrapper():
+        start_time = time.time()
+        funkcja()
+        end_time = time.time()
+        wynik = end_time - start_time
+        print(f"Czas wykonania funkcji: {wynik} s.")
+
+    return wrapper
