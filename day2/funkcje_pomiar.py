92fdd2a1d51e3c4b39a8616dbb310fe2a693f48e
rajkonkret <rajkonkret660@gmail.com>
2026-03-17 10:21:46 +0100

    [2 6] update funkcja_pomiar

---
+++ day2/funkcje_pomiar.py
@@ -0,0 +1,14 @@
+import time
+import operator
+from functools import partial
+
+
+def pomiarczasu(funkcja):
+    def wrapper():
+        start_time = time.time()
+        funkcja()
+        end_time = time.time()
+        wynik = end_time - start_time
+        print(f"Czas wykonania funkcji: {wynik} s.")
+
+    return wrapper
