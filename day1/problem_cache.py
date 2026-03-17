072d92c3c0365019019b6c60e29d2ad11cb354af
rajkonkret <rajkonkret660@gmail.com>
2026-03-17 09:17:41 +0100

    [1 21] update dekoratory.py

---
+++ day1/problem_cache.py
@@ -0,0 +1,96 @@
+# zasymulowac cache dla funkcji
+# jesli funkcja wywołąna z argumentami jakie znamy juz -> wynik z cache
+# w przeciwnym wypadku dokonac nowego obliczenia i zwrócic wynik
+
+def memoizacja(funkcja):
+    cache = {}
+    print("ID cache:", id(cache))
+
+    def wrapper(*args):
+        if args in cache:
+            print(f"Zwracamy wynik z cache dla argumentów: {args}")
+            print(f"Funkcja: {funkcja.__name__}({args} -> {funkcja(*args)})")
+            print(f"Używam cache o ID:", id(cache))
+            return cache[args]
+        else:
+            wynik = funkcja(*args)
+            cache[args] = wynik
+            print(cache)
+            return wynik
+
+    return wrapper
+
+
+@memoizacja
+def fiboacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fiboacci(n - 1) + fiboacci(n - 2)
+
+
+@memoizacja
+def fibonaci2(n):
+    if n <= 1:
+        return n
+    else:
+        return fiboacci(n - 1) + fiboacci(n - 2)
+
+print(58 * "-")
+print("Uzycie fibonacci 1")
+print(fiboacci(3))
+# Zwracamy wynik z cache dla argumentów: (1,)
+# Funkcja: fiboacci((1,) -> 1)
+# Zwracamy wynik z cache dla argumentów: (2,)
+# Zwracamy wynik z cache dla argumentów: (1,)
+# Funkcja: fiboacci((1,) -> 1)
+# Zwracamy wynik z cache dla argumentów: (0,)
+# Funkcja: fiboacci((0,) -> 0)
+
+print(58 * "-")
+print("Uzycie fibonacci 2")
+print(fiboacci(2))
+
+print(58 * "-")
+print("Uzycie fibonacci2")
+print(fibonaci2(3))
+# ID cache: 1765443170624
+# ID cache: 1765443575168
+# ----------------------------------------------------------
+# Uzycie fibonacci 1
+# {(1,): 1}
+# {(1,): 1, (0,): 0}
+# {(1,): 1, (0,): 0, (2,): 1}
+# Zwracamy wynik z cache dla argumentów: (1,)
+# Funkcja: fiboacci((1,) -> 1)
+# Używam cache o ID: 1765443170624
+# {(1,): 1, (0,): 0, (2,): 1, (3,): 2}
+# 2
+# ----------------------------------------------------------
+# Uzycie fibonacci 2
+# Zwracamy wynik z cache dla argumentów: (2,)
+# Zwracamy wynik z cache dla argumentów: (1,)
+# Funkcja: fiboacci((1,) -> 1)
+# Używam cache o ID: 1765443170624
+# Zwracamy wynik z cache dla argumentów: (0,)
+# Funkcja: fiboacci((0,) -> 0)
+# Używam cache o ID: 1765443170624
+# Funkcja: fiboacci((2,) -> 1)
+# Używam cache o ID: 1765443170624
+# 1
+# ----------------------------------------------------------
+# Uzycie fibonacci2
+# Zwracamy wynik z cache dla argumentów: (2,)
+# Zwracamy wynik z cache dla argumentów: (1,)
+# Funkcja: fiboacci((1,) -> 1)
+# Używam cache o ID: 1765443170624
+# Zwracamy wynik z cache dla argumentów: (0,)
+# Funkcja: fiboacci((0,) -> 0)
+# Używam cache o ID: 1765443170624
+# Funkcja: fiboacci((2,) -> 1)
+# Używam cache o ID: 1765443170624
+# Zwracamy wynik z cache dla argumentów: (1,)
+# Funkcja: fiboacci((1,) -> 1)
+# Używam cache o ID: 1765443170624
+# {(3,): 2}
+# 2
