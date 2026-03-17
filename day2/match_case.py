f2894b9c699c79014d191cb40690e8be014ddc1b
rajkonkret <rajkonkret660@gmail.com>
2026-03-17 15:38:21 +0100

    [2 47] update match_case.py

---
+++ day2/match_case.py
@@ -0,0 +1,18 @@
+# od 3.10
+
+lang = input("Podaj imię")
+
+match lang.strip().casefold():
+    case "radek":
+        print("OK")
+    case "tomek":
+        print("Tomek")
+    case _:  # else
+        print("inny")
+
+name1 = "GROSS"
+name2 = "groß"
+
+print(name1.lower() == name2.lower())  # False
+""" Return a version of the string suitable for caseless comparisons. """
+print(name1.casefold() == name2.casefold())  # True
