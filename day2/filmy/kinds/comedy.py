78f2ab75bc8d6ab405349a7334fad6187faad74d
rajkonkret <rajkonkret660@gmail.com>
2026-03-17 13:57:50 +0100

    [2 33 update comedy.py

--- day2/filmy/kinds/comedy.py
+++ day2/filmy/kinds/comedy.py
@@ -1,4 +1,18 @@
 # film, series
+from day2.filmy.film import Film
+from day2.filmy.series import SeriesPart
 
-class ComedyFilm:
-    ...
+
+class ComedyFilm(Film, SeriesPart):
+    def __init__(self, title: str, director: str, year: int, duration: float, series_name: str, part_number: int):
+        Film.__init__(self, title, director, year, duration)
+        SeriesPart.__init__(self, series_name, part_number)
+
+    def play(self):
+        print(f"Odtwarzanie komedii: {self.title}")
+
+    def get_inf(self):
+        info = f"{self.title}, reżyseria: {self.director}, produkcja: {self.year},  czas trwnia [h]: {self.duration}"
+        series_info = self.get_series_info()
+        info += f' | {series_info}'
+        return info
