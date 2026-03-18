ca048b2e88ad0d7012c242738834e4f2f38239f3
rajkonkret <52714027+rajkonkret@users.noreply.github.com>
2026-03-18 09:15:04 +0100

    Add CustomIntFloatDict with error handling classes
    Implement error handling for dictionary creation with specific key and value types.

--- day3/dict_error/main.py
+++ day3/dict_error/main.py
@@ -1,4 +1,9 @@
 """
 Przygotuj obsługę błędu dla następujących przypadków:
 Utwórz klasę CustomIntFloatDict za pomocą której bedziesz tworzyć nowe słowniki,
+zadając klucze i wartości w odrębnych kolekcjach - jedyne dopuszczalne kolekcje to: list i tuple
+typ wartości w słowniku to jedynie int lub float!
+Utwórz do obsługi błędu dwie klasy błędu:
+1. IntFloatValueError - reakcja na wartości słownika inne niż int i float
+2. KeyValueConstruktorError - reakcja na przekazanie kluczy i wartości w niewłasciym typie (tylko list i tuple)
 """
