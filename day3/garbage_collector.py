28eb5a81171f691be2eaff7379f5dd43feac9c73
rajkonkret <rajkonkret660@gmail.com>
2026-03-18 12:07:56 +0100

    [3 14] update garbage_collector.py

--- day3/garbage_collector.py
+++ day3/garbage_collector.py
@@ -13,3 +13,28 @@ class User:
         print(f'USUWAM obiekt: {self.name}')
 
 
+def normal_dict_example():
+    print("--- Zwykły dict ---")
+    d = {}
+
+    u = User("Jan")
+    d[u] = "dane Jana"
+
+    print("Przed usunięciem zmiennej")
+    print(d)
+
+    u = None
+    gc.collect()
+
+    print("Po u = None i gc.collect():")
+    print(d)
+    print("Obiekt nadal żyje")
+
+normal_dict_example()
+# --- Zwykły dict ---
+# Przed usunięciem zmiennej
+# {User('Jan'): 'dane Jana'}
+# Po u = None i gc.collect():
+# {User('Jan'): 'dane Jana'}
+# Obiekt nadal żyje
+# USUWAM obiekt: Jan
