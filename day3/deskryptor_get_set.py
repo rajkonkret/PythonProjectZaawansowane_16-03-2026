2290d051306cf4ef626ab6228f72e1bf268c12a6
rajkonkret <rajkonkret660@gmail.com>
2026-03-18 11:50:42 +0100

    [3 9] update deskryptor_get_set.py

---
+++ day3/deskryptor_get_set.py
@@ -0,0 +1,31 @@
+# deskryptor
+
+class LoggedPositiveInt:
+    def __init__(self):
+        self._values = {}
+
+    def __get__(self, instance, owner):
+        print(f"{instance}")
+
+    def __set__(self, instance, value):
+        print("Ustawiasz pole")
+
+
+class Product:
+    stock = LoggedPositiveInt()
+
+    def __init__(self, stock):
+        self.stock = stock
+
+
+p1 = Product(10)
+p2 = Product(5)
+# Ustawiasz pole
+# Ustawiasz pole
+
+print(p1.stock)  # <__main__.Product object at 0x00000136BF408D70>
+p1.stock = 20
+print(p2.stock)
+# Ustawiasz pole
+# <__main__.Product object at 0x000001FB110B8A50>
+# None
