c781a54d396e10530c2fd7f354130f411bab754b
rajkonkret <rajkonkret660@gmail.com>
2026-03-18 14:32:41 +0100

    [3 31] update order_system

--- day3/order_system/order.py
+++ day3/order_system/order.py
@@ -1,5 +1,33 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from customer import Customer
 from orderitem import OrderItem
 from typing import List
 import datetime
+
+
+@dataclass
+class Order:
+    customer: Customer
+    items: List[OrderItem] = field(default_factory=list)
+    order_date: datetime.datetime = field(default_factory=datetime.datetime.now)
+    status: str = "Pending"
+
+    def add_item(self, item: OrderItem):
+        if item.product.is_available(item.quantity):
+            item.product.stock -= item.quantity
+            self.items.append(item)
+        else:
+            raise ValueError(f"Produkt {item.product.name} jest niedosstępny żądanej ilości")
+
+    def total_order_value(self) -> float:
+        return sum(item.total_price() for item in self.items)
+
+    def summary(self) -> str:
+        item_details = "\n".join(f"{item.product.anme} x {item.quantity} - {item.total_price()} zł"
+                                 for item in self.items)
+
+        return (f"Zamówienie dla: {self.customer.full_name()}: \n"
+                f"Data zamówienia: {self.order_date}\n"
+                f"Status: {self.status} \n"
+                f"Pozycje: \n {item_details}\n"
+                f"Łączna kwota: {self.total_order_value()} zł")
