7e383f02c60c0372e20a2485d0ed6a7bec326200
rajkonkret <rajkonkret660@gmail.com>
2026-03-18 12:27:06 +0100

    [3 17] update garbage_collector.py

--- day3/zastosowanie_gettattribute__.py
+++ day3/zastosowanie_gettattribute__.py
@@ -1,7 +1,7 @@
 class SecureData:
     def __init__(self, public, secret):
         self.public = public
-        self._secet = secret
+        self._secret = secret
 
     @property
     def info(self):
@@ -14,3 +14,28 @@ class SecureData:
             raise AttributeError("Brak dostępu do _secret")
 
         return super().__getattribute__(name)
+
+
+print("--- Tworzenie obiektu ---")
+obj = SecureData("widoczne dane", "tajne dane")
+
+print("--- Normalny atrybut ---")
+print(obj.public)
+
+print("--- Odczyt propery ---")
+print(obj.info)
+# --- Normalny atrybut ---
+# [LOG] Próba dostępu do atrybutu: public
+# widoczne dane
+# --- Odczyt propery ---
+# [LOG] Próba dostępu do atrybutu: info
+# [LOG] Próba dostępu do atrybutu: public
+# PUBLIC=widoczne dane
+
+print("--- Zablokowany element ---")
+try:
+    print(obj._secret)
+except AttributeError as e:
+    print("Bład:", e)
+# [LOG] Próba dostępu do atrybutu: _secret
+# Bład: Brak dostępu do _secret
