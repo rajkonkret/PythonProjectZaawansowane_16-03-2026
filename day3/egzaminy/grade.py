ac9a0d0af6ada6eeb276db77f4c90a89a8b6d7fb
rajkonkret <rajkonkret660@gmail.com>
2026-03-18 13:31:54 +0100

    [3 22] update klas_danych_property

---
+++ day3/egzaminy/grade.py
@@ -0,0 +1,17 @@
+class Grade:
+    def __init__(self):
+        self._value = 0
+
+    def __get__(self, instance, owner):
+        return self._value
+
+    def __set__(self, instance, value):
+        if not (0 <= value <= 100):
+            raise ValueError("Ocena musi być wartością z przedziało 0-100!!!")
+        self._value = value
+
+
+class ExamG:
+    math_grade = Grade()
+    alg_grade = Grade()
+    prog_grade = Grade()
