83c5c21484e74ec16ac6581c0e922987bdf63f95
rajkonkret <rajkonkret660@gmail.com>
2026-03-19 09:41:55 +0100

    [4 7] update typowanie.py

--- day4/typowanie.py
+++ day4/typowanie.py
@@ -95,3 +95,19 @@ start_race(robot)
 # 1.15
 # robot is running
 # 1.12
+
+# generic
+from typing import TypeVar, Generic, List
+
+T = TypeVar("T")
+
+
+class Stack(Generic[T]):
+    def __init__(self) -> None:
+        self._container: List[T] = []
+
+    def push(self, item: T) -> None:
+        self._container.append(item)
+
+    def pop(self) -> T:
+        return self._container.pop()
