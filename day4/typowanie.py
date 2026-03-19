2ab11b82b2f577ed78209c8db8dcddfe29d5b355
rajkonkret <rajkonkret660@gmail.com>
2026-03-19 09:19:48 +0100

    [4 1] update typowanie.py

---
+++ day4/typowanie.py
@@ -0,0 +1,23 @@
+# użycie typing
+from typing import TypedDict, get_origin
+
+
+class User(TypedDict):
+    name: str
+    age: int
+    email: str
+
+
+def print_user_info(user: User) -> None:
+    print(f"Name: {user['name']}, Age: {user['age']}, e-mail: {user['email']}")
+
+
+user_data = {'name': "Leon", 'age': 40, 'email': 'leon@abc.pl'}
+print_user_info(user_data)  # Name: Leon, Age: 40, e-mail: leon@abc.pl
+
+# złożone aliasy typów
+from typing import List, Tuple, Union
+
+Coordinate = Tuple[float, float]
+Path = List[Coordinate]
+CoordinateError = Union[Coordinate, int]
