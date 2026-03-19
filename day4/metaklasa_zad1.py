34e7e2c84ed2cf2adc7d41c68723c6b5c8bfefb7
rajkonkret <rajkonkret660@gmail.com>
2026-03-19 13:30:52 +0100

    [4 27] metaprogramowanie

---
+++ day4/metaklasa_zad1.py
@@ -0,0 +1,12 @@
+# metaklasa
+
+
+class User:
+    pass
+
+
+print(type(User))  # <class 'type'> podstawowa klasa w pythonie -> metaklasa
+print(type(User()))  # <class '__main__.User'>
+
+x = 5
+print(type(x))  # <class 'int'>
