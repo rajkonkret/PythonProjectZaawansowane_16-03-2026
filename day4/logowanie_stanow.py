7126fc0809719b2da552af3a9086d8d6ec691fde
rajkonkret <rajkonkret660@gmail.com>
2026-03-19 11:27:51 +0100

    [4 18] logowanie_stanow.py

--- day4/logowanie_stanow.py
+++ day4/logowanie_stanow.py
@@ -1,5 +1,33 @@
 """
 dekorator do logowaia stanu obiektów
 Można sledzic zmiany stanó obiektów i kontrolowac jakie metody na nie wpływaja
-__setattribute__
+__setattr__
 """
+
+
+def log_state_changes(cls):
+    class Wrappped(cls):
+        def __setattr__(self, key, value):
+            if key in self.__dict__:
+                old_value = self.__dict__[key]
+                print(f"Zmieniono {key} z {old_value} to {value}")
+            super().__setattr__(key, value)
+
+    return Wrappped
+
+
+@log_state_changes
+class Document:
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+    def update_content(self, new_content):
+        self.content = new_content
+
+
+doc = Document("Python Tips", "Content about Python!")
+doc.update_content("new big content about Python libraries")
+print(doc.content)
+# Zmieniono content z Content about Python! to new big content about Python libraries
+# new big content about Python libraries
