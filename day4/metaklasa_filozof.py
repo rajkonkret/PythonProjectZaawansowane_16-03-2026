c27298bd09caa235282d02452071917482c77151
rajkonkret <rajkonkret660@gmail.com>
2026-03-19 15:34:36 +0100

    [4 42] update metaprogramowanie

--- day4/metaklasa_filozof.py
+++ day4/metaklasa_filozof.py
@@ -26,3 +26,34 @@ class SednoOdpowiedzi(type):
                 cls.odpowiedz = odpowiedz
         else:
             cls.odpowiedz = brak
+
+
+class Arystoteles(metaclass=SednoOdpowiedzi):
+    pass
+
+
+class Platon(metaclass=SednoOdpowiedzi):
+    pass
+
+
+class SwTomasz(metaclass=SednoOdpowiedzi):
+    n = False
+
+
+fil1 = Arystoteles()
+print(f"Filozof {fil1.__class__.__name__} twierdzi: {fil1.odpowiedz()}")
+# Czy ziemia jest płaska? Czy chcesz znać odpowiedź? (t/n): t
+# Filozof Arystoteles twierdzi: Tak! Ziemia jest płaska
+
+fil2 = Platon()
+print(f"Filozof {fil2.__class__.__name__} twierdzi: {fil2.odpowiedz()}")
+# Filozof Arystoteles twierdzi: Tak! Ziemia jest płaska
+# Filozof Platon twierdzi: Tak! Ziemia jest płaska
+
+fil3 = SwTomasz()
+print(f"Filozof {fil3.__class__.__name__} twierdzi: {fil3.odpowiedz()}")
+# Filozof SwTomasz twierdzi: Tak! Ziemia jest płaska
+
+# zadanie1:
+# Klasa Kopernik
+# ma odpowiadac: "Nie! Ziemia jest elipsoidą"
