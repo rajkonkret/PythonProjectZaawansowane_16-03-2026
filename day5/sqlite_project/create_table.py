4af0f15fe7d34bb95ff8b7a367295a2eff47b62c
rajkonkret <rajkonkret660@gmail.com>
2026-03-20 11:17:14 +0100

    [5 114] xml filmy sax

---
+++ day5/sqlite_project/create_table.py
@@ -0,0 +1,10 @@
+from sqlite3 import Error
+from conn import create_connection
+
+
+def create_table(conn, create_table_sql):
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
