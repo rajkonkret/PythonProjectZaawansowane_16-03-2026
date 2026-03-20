0b7978816efbed430fc856423ee19758bf31c257
rajkonkret <rajkonkret660@gmail.com>
2026-03-20 10:07:59 +0100

    [5 4] update metaprogramowanie

---
+++ day5/XML/main.py
@@ -0,0 +1,35 @@
+from xml.etree.ElementTree import Element, SubElement
+import xml.etree.ElementTree as ET
+
+top = Element('autokomis')
+
+# pierwszy samochód
+sam1 = SubElement(top, 'samochod')
+
+id = SubElement(sam1, "id")
+id.text = 'sam1'
+
+marka = SubElement(sam1, 'marka')
+marka.text = 'Subaru'
+
+model = SubElement(sam1, 'model')
+model.text = 'Impreza'
+
+poj = SubElement(sam1, 'pojemosc')
+poj.text = '2.0'
+
+rocznik = SubElement(sam1, "rocznik")
+rocznik.text = '2000'
+
+cena = SubElement(sam1, 'cena')
+cena.text = '64000'
+
+wyp_dod = SubElement(sam1, 'wyposazenie_dod')
+
+kolor = SubElement(wyp_dod, 'kolor')
+kolor.text = 'czarna perła'
+
+klima = SubElement(wyp_dod, "klimatyzacja")
+klima.text = "jest"
+
+print(top)
