4e39defedd1f524d89a8fcc874e7d5d4034c5734
rajkonkret <rajkonkret660@gmail.com>
2026-03-20 12:24:05 +0100

    [5 28] update orm

--- day5/orm_pewee_zad1.py
+++ day5/orm_pewee_zad1.py
@@ -1,4 +1,10 @@
+import logging
 from peewee import *
+
+# dodanie logowania
+logger = logging.getLogger("peewee")
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.DEBUG)
 
 db = SqliteDatabase("app.db")
 
@@ -21,5 +27,19 @@ User.create(name="Anna", age=22)
 
 for user in User.select():
     print(user.name, user.age)
+# Jan 30
+# Anna 22
+# ('CREATE TABLE IF NOT EXISTS "user" ("id" INTEGER NOT NULL PRIMARY KEY, "name" VARCHAR(255) NOT NULL,
+# "age" INTEGER NOT NULL)', [])
+# ('INSERT INTO "user" ("name", "age") VALUES (?, ?)', ['Jan', 30])
+# ('INSERT INTO "user" ("name", "age") VALUES (?, ?)', ['Anna', 22])
+# ('SELECT "t1"."id", "t1"."name", "t1"."age" FROM "user" AS "t1"', [])
+
+print("age > 20")
+# ('SELECT "t1"."id", "t1"."name", "t1"."age" FROM "user" AS "t1" WHERE ("t1"."age" > ?)', [20])
+users = User.select().where(User.age > 20)
+for u in users:
+    print(u.name, u.age)
+# age > 20
 # Jan 30
 # Anna 22
