75bcc1434ff5133cd960f20c0d2c3895cd27ffe4
rajkonkret <rajkonkret660@gmail.com>
2026-03-20 14:21:10 +0100

    [5 39] update prime

--- day5/prime/funkcja_prime.py
+++ day5/prime/funkcja_prime.py
@@ -2,7 +2,7 @@ def find_the_sum_pime_nums(minimum, maximum):
     total = 0
     for number in range(minimum, maximum + 1):
         count = 0
-        for i in range(2, (number / / 2 + 1)):
+        for i in range(2, (number // 2 + 1)):
             if number % i == 0:
                 count += 1
                 break
