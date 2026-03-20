6ff12c71af5d6543e234ba05a5d4fe8ecc2517b4
rajkonkret <rajkonkret660@gmail.com>
2026-03-20 10:16:47 +0100

    [5 7] xml subaru

--- day5/XML/main.py
+++ day5/XML/main.py
@@ -33,6 +33,38 @@ kolor.text = 'czarna perła'
 klima = SubElement(wyp_dod, "klimatyzacja")
 klima.text = "jest"
 
+# drugi samochod
+sam1 = SubElement(top, 'samochod')
+
+id = SubElement(sam1, "id")
+id.text = 'sam2'
+
+marka = SubElement(sam1, 'marka')
+marka.text = 'Subaru'
+
+model = SubElement(sam1, 'model')
+model.text = 'Outback'
+
+poj = SubElement(sam1, 'pojemosc')
+poj.text = '2.5'
+
+rocznik = SubElement(sam1, "rocznik")
+rocznik.text = '2021'
+
+cena = SubElement(sam1, 'cena')
+cena.text = '123000'
+
+wyp_dod = SubElement(sam1, 'wyposazenie_dod')
+
+kolor = SubElement(wyp_dod, 'kolor')
+kolor.text = 'cerwony metalik'
+
+klima = SubElement(wyp_dod, "klimatyzacja")
+klima.text = "jest"
+
+pod = SubElement(wyp_dod, "poduszki")
+pod.text = "4"
+
 print(top)
 print(pretty(top))
 
