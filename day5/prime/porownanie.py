d8c908b542847fa9360733a3e7d093a54e8f495b
rajkonkret <rajkonkret660@gmail.com>
2026-03-20 14:23:29 +0100

    [5 40] update prime

---
+++ day5/prime/porownanie.py
@@ -0,0 +1,9 @@
+import time
+import concurrent.futures
+import multiprocessing
+
+from funkcja_prime import find_the_sum_pime_nums
+
+numbers = [(1, 10_000), (3, 50_000), (5_000, 100_000), (4, 9_000), (8_000, 15_000), (95_000, 133_000, (200, 67_540))]
+
+
