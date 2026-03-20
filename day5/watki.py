d126d940f227e9476095914127e17ba7ad3cb89c
rajkonkret <rajkonkret660@gmail.com>
2026-03-20 14:47:47 +0100

    [5 45] update prime

---
+++ day5/watki.py
@@ -0,0 +1,24 @@
+import threading
+
+
+def print_cube(num):
+    print(f"Sześcian wrtości {num} wynosi: {num ** 3}")
+
+
+def print_square(num):
+    print(f'Kwadrat wartości {num} wynosi: {num ** 2}')
+
+
+t1 = threading.Thread(target=print_cube, args=(1000,))
+t2 = threading.Thread(target=print_square, args=(100,))
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
+
+print("Zakończone")
+# Sześcian wrtości 1000 wynosi: 1000000000
+# Kwadrat wartości 100 wynosi: 10000
+# Zakończone
