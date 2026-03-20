655af5a7e97e5731c81f81fb38819e4f93f66980
rajkonkret <rajkonkret660@gmail.com>
2026-03-20 10:24:48 +0100

    [5 9] xml subaru

--- day5/XML/read_xml.py
+++ day5/XML/read_xml.py
@@ -1,7 +1,8 @@
 import xml.etree.ElementTree as ET
 
 try:
-    tree = ET.parse('subaru_komis.xml')
+    # tree = ET.parse('subaru_komis.xml')
+    tree = ET.parse('kraj.xml')
     root = tree.getroot()
 
     for child in root:
@@ -17,3 +18,11 @@ except Exception as e:
 # <Element 'samochod' at 0x000001F235118EF0>
 # <Element 'marka' at 0x000001F235118F90>
 # Subaru
+
+#
+# tag: country, atrybuty: {'name': 'Liechtenstein', 'continent': 'Europa'}
+# tag: country, atrybuty: {'name': 'Panama', 'continent': 'Ameryka Środkowa'}
+# tag: country, atrybuty: {'name': 'Polska', 'continent': 'Europa'}
+# <Element 'country' at 0x000001FDF7948EF0>
+# <Element 'rok' at 0x000001FDF7948F90>
+# 2020
