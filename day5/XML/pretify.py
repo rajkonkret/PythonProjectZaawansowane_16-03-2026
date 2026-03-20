1fa0ae91c1bd1a8785e5b1ede9ad6d927ca80084
rajkonkret <rajkonkret660@gmail.com>
2026-03-20 10:10:14 +0100

    [5 5] update metaprogramowanie

---
+++ day5/XML/pretify.py
@@ -0,0 +1,8 @@
+from xml.etree import ElementTree
+from xml.dom import minidom
+
+
+def pretty(element):
+    xml_string = ElementTree.tostring(element, 'utf-8')
+    rep = minidom.parseString(xml_string)
+    return rep.toprettyxml(indent="   ")
