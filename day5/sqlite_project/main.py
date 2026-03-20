eba62f545f113fbbbcba454509f5037ef506e70f
rajkonkret <rajkonkret660@gmail.com>
2026-03-20 12:00:16 +0100

    [5 25] update insert_data.py

--- day5/sqlite_project/main.py
+++ day5/sqlite_project/main.py
@@ -1,8 +1,20 @@
 from conn import create_connection
 from create_table import cr
 from inserrt_data import insert
+from select_data import select
 
 if __name__ == '__main__':
     # create_connection("mojabaza1.db")
     # cr()
-    insert()
+    # insert()
+    select()
+# 1. Wszystkie zadania:
+# (1, 'Analiza wymagań dotyczących aplikacji', 1, 1, 3, '2023-12-27', '2023-12-30')
+# (2, 'Przygotowanie diagramów UML', 2, 1, 3, '2024-01-03', '2024-01-15')
+# (3, 'Analiza wymagań dotyczących aplikacji', 1, 1, 4, '2024-01-02', '2024-01-05')
+# (4, 'Przygotowanie modelu sieci neuronowej', 3, 1, 4, '2024-02-03', '2024-02-15')
+# 2. Wszystkie zadania:
+# (1, 'Analiza wymagań dotyczących aplikacji', 1, 1, 3, '2023-12-27', '2023-12-30')
+# (2, 'Przygotowanie diagramów UML', 2, 1, 3, '2024-01-03', '2024-01-15')
+# (3, 'Analiza wymagań dotyczących aplikacji', 1, 1, 4, '2024-01-02', '2024-01-05')
+# (4, 'Przygotowanie modelu sieci neuronowej', 3, 1, 4, '2024-02-03', '2024-02-15')
