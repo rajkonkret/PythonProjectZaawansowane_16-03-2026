b4c57ab3d0df3d46408510456455f9d52e8902e8
rajkonkret <rajkonkret660@gmail.com>
2026-03-20 11:14:59 +0100

    [5 13] xml filmy sax

---
+++ day5/sqlite_project/conn.py
@@ -0,0 +1,14 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+    return conn
---
+++ day5/sqlite_project/main.py
@@ -0,0 +1,4 @@
+from conn import create_connection
+
+if __name__ == '__main__':
+    create_connection("mojabaza1.db")
