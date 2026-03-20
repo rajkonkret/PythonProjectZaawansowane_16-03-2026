51f4290a27c8fd364ba897642fe0e042cfa4fa97
rajkonkret <rajkonkret660@gmail.com>
2026-03-20 11:34:55 +0100

    [5 20] update create_table.py

--- day5/sqlite_project/inserrt_data.py
+++ day5/sqlite_project/inserrt_data.py
@@ -1,5 +1,6 @@
 from conn import create_connection
 
+
 def create_project(conn, project):
     sql = """
     INSERT INTO projects(name,begin_date,end_date) VALUES(?,?,?);
@@ -7,5 +8,16 @@ def create_project(conn, project):
 
     cur = conn.cursor()
     cur.execute(sql, project)
+    conn.commit()
+    return cur.lastrowid
+
+
+def create_task(conn, task):
+    sql = """
+    INSERT INTO task(name,priority,status_id,project_id,begin_date,end_date) VALUES(?,?,?,?,?,?);
+    """
+
+    cur = conn.cursor()
+    cur.execute(sql, task)
     conn.commit()
     return cur.lastrowid
